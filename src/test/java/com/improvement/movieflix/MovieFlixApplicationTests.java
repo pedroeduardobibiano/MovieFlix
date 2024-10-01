fe23e2a3da5e46de52feb91501baa8f7d9776233
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2024-10-01 20:59:53 -0300

    Project MovieFlix Created

---
+++ src/test/java/com/improvement/movieflix/MovieFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.improvement.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFlixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
